df2f36e8bcba49b02ff46979bc35f4892fe46139
Dennis Kenneth <dennyify123@gmail.com>
2024-06-05 17:09:34 +0000

    DB migrated

--- network/models.py
+++ network/models.py
@@ -17,13 +17,13 @@ class Post(models.Model):
 class Like(models.Model):
     user: models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_like")
     post: models.ForeignKey(Post, on_delete=models.CASCADE, related_name="post_like")
-    timestamp: models_DateTimeField(auto_now=True)
+    timestamp: models.DateTimeField(auto_now=True)
 
     def __str__(self) :
         return f"Like {self.id} by {self.user} on post {self.post.id}"
     
 class Follow(models.Model):
-    user: models.ForeignKey(User, on_delete=models_CASCADE, related_name="user_following")
+    user: models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_following")
     followed: models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_followed")
     timestamp = models.DateTimeField(auto_now_add=True)
 
